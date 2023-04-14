f2346f5de143a8c9ebdf0cae84d6cc203ea15f1e
Shubham Joshi <115713904+sjoshi91119@users.noreply.github.com>
2023-04-14 10:12:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,41 +1,24 @@
-//your JS code here. If required.
-  const form = document.querySelector('form');
-  const nameInput = document.querySelector('#name');
-  const ageInput = document.querySelector('#age');
-  const btn = document.querySelector('#btn');
+const nameInput = document.getElementById('name');
+const ageInput = document.getElementById('age');
+const submitBtn = document.getElementById('btn');
 
-  btn.addEventListener('click', (event) => {
-    event.preventDefault(); // prevent the default form submission behavior
-
-    // check if the inputs are not empty
-    if (!nameInput.value || !ageInput.value) {
-      alert('Please enter both name and age.');
-      return;
-    }
-
-    // create a promise to handle the form submission
-    const formPromise = new Promise((resolve, reject) => {
-      // set a timeout to simulate asynchronous processing
-      setTimeout(() => {
-        const age = parseInt(ageInput.value);
-
-        // check if the age is above 18 and resolve the promise
-        if (age > 18) {
-          const welcomeMessage = `Welcome, ${nameInput.value}. You can vote.`;
-          alert(welcomeMessage);
-          resolve(welcomeMessage);
-        }
-        // otherwise, reject the promise
-        else {
-          const sorryMessage = `Oh sorry ${nameInput.value}. You aren't old enough.`;
-          alert(sorryMessage);
-          reject(sorryMessage);
-        }
-      }, 4000);
-    });
-
-    // handle the promise resolution and rejection
-    formPromise
-      .then((result) => console.log(result))
-      .catch((error) => console.log(error));
+submitBtn.addEventListener('click', (event) => {
+  event.preventDefault(); 
+  const delayPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Number(ageInput.value) >= 18) {
+        resolve({ name: nameInput.value, age: Number(ageInput.value) });
+      } else {
+        reject({ name: nameInput.value });
+      }
+    }, 4000);
   });
+
+  delayPromise
+    .then((data) => {
+      alert(`Welcome, ${data.name}. You can vote.`);
+    })
+    .catch((error) => {
+      alert(`Oh sorry ${error.name}. You aren't old enough.`);
+    });
+});
